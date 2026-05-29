0f721a6f075b1b4e601594cfa3cdfca247cbfcec
Niaz-Tan <nuddintanim@gmail.com>
2026-05-29 15:03:58 +0600

    refector: banner, navbar background color

--- app/(main)/_components/homePage/HomeBanner.js
+++ app/(main)/_components/homePage/HomeBanner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HomeBanner = () => {
   return (
-    <div className="relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] overflow-hidden">
+    <div className="relative w-full h-50 sm:h-75 md:h-100 lg:h-125 xl:h-150 overflow-hidden">
       <Image
         src="/pouchBanner.jpg"
         alt="banner"
