f7c33b223aceb2d5289e8fe28a77238fc383f32f
Niaz Uddin <nuddintanim@gmail.com>
2026-06-07 20:30:33 +0600

    feat: added new cart model

---
+++ model/cart-model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const CartItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    color: {
+      type: String,
+      default: null,
+    },
+  },
+  { _id: false },
+);
+
+const CartSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+      unique: true,
+    },
+
+    items: {
+      type: [CartItemSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Cart = mongoose.models.Cart || mongoose.model("Cart", CartSchema);
