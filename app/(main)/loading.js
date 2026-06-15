1e878000d0b40ec7192ef6c1757957f16bf12778
Niaz Uddin <nuddintanim@gmail.com>
2026-06-15 23:39:49 +0600

    formating

--- app/(main)/loading.js
+++ app/(main)/loading.js
@@ -3,10 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div>
-      
       <Skeleton className="h-75 sm:h-100 md:h-125 lg:h-150 w-full bg-[#DCCFBD]" />
 
-      {/* Products Section */}
       <section className="px-4 py-14">
         <div className="mx-auto max-w-7xl">
           <div className="mb-12 flex flex-col items-center gap-4">
